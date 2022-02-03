b79da8dff29cbb70dedbe845f18e84220dd3298d
Kiril <kiril@videlov.com>
2022-02-03 10:43:54 +0000

    Update test to use uppercase
    --- sturdy ---
    user_id: 2be622cc-03ce-41fc-a2fa-13daff3d1d53
    view_id: acf6fb4b-0008-45cb-86e7-ec7a69e13c80
    workspace_id: 76a382a3-4991-4b8d-9b0c-2fa27da75e10

--- pkg/fizzbuzz/fizzbuzz_test.go
+++ pkg/fizzbuzz/fizzbuzz_test.go
@@ -9,13 +9,13 @@ import (
 func TestUpToFive(t *testing.T) {
 	out := UpToLength(5)
 	assert.ElementsMatch(t, out,
-		[]string{"1", "2", "Fizz", "4", "Buzz"})
+		[]string{"1", "2", "FIZZ", "4", "BUZZ"})
 }
 
 func TestUpToFifteen(t *testing.T) {
 	out := UpToLength(15)
 	assert.ElementsMatch(t, out,
-		[]string{"1", "2", "Fizz", "4", "Buzz",
-			"Fizz", "7", "8", "Fizz", "Buzz",
-			"11", "Fizz", "13", "14", "FizzBuzz"})
+		[]string{"1", "2", "FIZZ", "4", "BUZZ",
+			"FIZZ", "7", "8", "FIZZ", "BUZZ",
+			"11", "FIZZ", "13", "14", "FIZZBUZZ"})
 }
